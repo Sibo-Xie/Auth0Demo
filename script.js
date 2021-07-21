ccfa545ec13172054bc5451f68ce32718e15fb77
Sibo-Xie <superwestwood@hotmail.com>
2021-07-21 20:08:05 +0900

    Try again

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ const updateUI = async () => {
     document.getElementById("ipt-user-profile-image").src = profile.picture;
     
     //ログイン所在国
-    document.getElementById("ipt-user-profile-country").innerHTML = profile["https://example.com/country_name"];
+    const strCountryVarName = "https://example.com/country_name";
+    document.getElementById("ipt-user-profile-country").innerHTML = profile[strCountryVarName];
 
   } else {
     document.getElementById("gated-content").classList.add("hidden");
